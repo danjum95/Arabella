a69cf05763d9c07ced50966ab2fb15ed057dc583
Dawid Majsnerowski <dawid.majsnerowski@wp.pl>
2018-05-21 20:18:52 +0200

    Routes WORKS!

--- Arabella/www/src/main/resources/templates/src/app/app.module.ts
+++ Arabella/www/src/main/resources/templates/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CalendarModule } from 'angular-calendar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Routes, RouterModule } from '@angular/router';
+
 
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
@@ -10,6 +12,14 @@ import { LoginComponent } from './login/login.component';
 import { UsersComponent } from './users/users.component';
 import { HeaderComponent } from './header/header.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'users', component: UsersComponent }
+];
+
 
 @NgModule({
   declarations: [
@@ -20,7 +30,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
     HeaderComponent,
     WelcomeComponent,
   ],
-  imports: [
+  imports: [RouterModule.forRoot(routes),
     BrowserModule,
     FormsModule
   ],
