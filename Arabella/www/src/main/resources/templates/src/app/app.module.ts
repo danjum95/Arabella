5c7bcd43351eb665ce8ab4a68d13e171eb4dc541
Dawid Majsnerowski <dawid.majsnerowski@wp.pl>
2018-05-23 21:04:27 +0200

    Template calendar

--- Arabella/www/src/main/resources/templates/src/app/app.module.ts
+++ Arabella/www/src/main/resources/templates/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 import { CalendarModule } from 'angular-calendar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Routes, RouterModule } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-
-import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
@@ -35,7 +35,8 @@ const routes: Routes = [
   ],
   imports: [RouterModule.forRoot(routes),
     BrowserModule,
-    FormsModule
+    FormsModule,
+    CalendarModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Arabella/www/src/main/resources/templates/src/app/calendar/calendar.component.spec.ts
+++ Arabella/www/src/main/resources/templates/src/app/calendar/calendar.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CalendarModule } from 'angular-calendar';
 import { CalendarComponent } from './calendar.component';
 
 describe('CalendarComponent', () => {
