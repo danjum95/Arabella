1e6693e10b284b5b3af8882a99bd4c5ffe115895
Dawid Majsnerowski <dawid.majsnerowski@wp.pl>
2018-06-08 17:57:02 +0200

    Token generate

--- Arabella/www/src/main/angular/src/app/login/login.component.ts
+++ Arabella/www/src/main/angular/src/app/login/login.component.ts
@@ -1,38 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+ 
+ 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  usersCount : number = 4;
-  btnText : string = 'Zaloguj';
-  goalText : string = 'Podaj swój e-mail';
-
+ 
+  usersCount: number = 4;
+  btnText: string = 'Zaloguj';
+  goalText: string = 'Podaj swój e-mail';
+ 
   constructor(private http: HttpClient) { }
-
+ 
   ngOnInit() {
   }
-
-  sendData()
-  {
-
-	 interface LoginResponse {
-	  token: string;
-	  accessExpiration: number;
-	}
-
-	var mail = ((document.getElementById("email") as HTMLInputElement).value);
-	let data = {email: mail};
-
-  	    this.http.post<LoginResponse>('http://orlean.ski:8090/api/login',
-		 data).
-		 subscribe(data => { console.log(data.token);
-		});
-		
+ 
+  sendData() {
+ 
+    interface LoginResponse {
+      token: string;
+      accessExpiration: number;
+    }
+ 
+    var mail = ((document.getElementById("email") as HTMLInputElement).value);
+    let data = { email: mail };
+    let headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');
+ 
+    this.http.post<LoginResponse>('http://orlean.ski:8090/api/login', data, { headers })
+    .subscribe(data => {
+        console.log(data.token);
+      });
+ 
   }
 }
+ 
