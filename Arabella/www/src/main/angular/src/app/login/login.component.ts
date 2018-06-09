fb58d365f7d12e1dba022dc4ccecee8f9216ed79
Dawid Majsnerowski <dawid.majsnerowski@wp.pl>
2018-06-09 09:29:06 +0200

    Cookie - Token

--- Arabella/www/src/main/angular/src/app/login/login.component.ts
+++ Arabella/www/src/main/angular/src/app/login/login.component.ts
@@ -12,7 +12,7 @@ export class LoginComponent implements OnInit {
   usersCount: number = 4;
   btnText: string = 'Zaloguj';
   goalText: string = 'Podaj swój e-mail';
- 
+  
   constructor(private http: HttpClient, private _cookieService:CookieService) { }
  
   ngOnInit() {
@@ -20,20 +20,19 @@ export class LoginComponent implements OnInit {
   }
  
   sendData() {
- 
+
     interface LoginResponse {
       token: string;
-      accessExpiration: number;
     }
- 
     var mail = ((document.getElementById("email") as HTMLInputElement).value);
     let data = { email: mail };
     let headers = new HttpHeaders();
+    let log = "";
     headers.append('Content-Type', 'application/json');
  
     this.http.post<LoginResponse>('http://orlean.ski:8090/api/login', data, { headers })
-    .subscribe(data => {
-        console.log(data.token);
+    .subscribe(data => { log = data.token;
+        console.log(log);
       });
   }
 }
