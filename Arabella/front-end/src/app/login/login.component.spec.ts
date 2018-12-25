b43f5ea1a04838ea4f074f5ae5f2eb3337dc2aad
Dawid Majsnerowski <dawid.majsnerowski@wp.pl>
2018-12-25 21:17:31 +0100

    Tests for login

--- Arabella/front-end/src/app/login/login.component.spec.ts
+++ Arabella/front-end/src/app/login/login.component.spec.ts
@@ -1,14 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { BrowserModule, By} from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {DebugElement} from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [HttpClientTestingModule, BrowserModule, FormsModule, ReactiveFormsModule,RouterTestingModule],
     })
     .compileComponents();
   }));
@@ -22,4 +29,20 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('form invalid when empty', () => {
+    expect(component.loginForm.valid).toBeFalsy();
+  });
+
+  it('form invalid', async(() => {
+    component.loginForm.controls['username'].setValue('');
+    component.loginForm.controls['password'].setValue('');
+    expect(component.loginForm.valid).toBeFalsy();
+  }));
+
+  it('form is valid', async(() => {
+    component.loginForm.controls['username'].setValue('kurs@kurs.pl');
+    component.loginForm.controls['password'].setValue('1234567');
+    expect(component.loginForm.valid).toBeTruthy();
+  }));
 });
