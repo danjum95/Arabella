db218a180f8f56c52f78a96b00266b523585a773
Dawid Majsnerowski <dawid.majsnerowski@wp.pl>
2018-12-30 15:36:27 +0100

    Test for backend

--- Arabella/src/test/java/arabella/backend/endpoint/LoginRestControllerTest.java
+++ Arabella/src/test/java/arabella/backend/endpoint/LoginRestControllerTest.java
@@ -1,29 +1,54 @@
 package arabella.backend.endpoint;
 
 import arabella.backend.model.User;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
 import static org.junit.Assert.*;
-import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(LoginRestController.class)
 public class LoginRestControllerTest {
-    @Autowired
+
     private MockMvc mvc;
 
+    @Autowired
+    private WebApplicationContext wac;
+
     @MockBean
-    private LoginRestController loginRestController;
+    private LoginRestController login;
+
+    @Before
+    public void init() {
+        mvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
 
     @Test
     public void loginCorrect() throws Exception {
-        User user = new User();
-        user.setEmail("test@test.pl");
-        user.setPassword("12345678");
+        String req = "{\"email\": \"student@student.pl\",\"password\": \"student\"}";
+        mvc.perform(post("/api/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(req)
+        )
+                .andExpect(status().isOk())
     }
 
 }
--- Arabella/src/test/java/arabella/backend/endpoint/MessageRestControllerTest.java
+++ Arabella/src/test/java/arabella/backend/endpoint/MessageRestControllerTest.java
@@ -1,7 +1,32 @@
 package arabella.backend.endpoint;
 
-import static org.junit.Assert.*;
+import arabella.backend.model.Message;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(MessageRestController.class)
 public class MessageRestControllerTest {
 
+    private MockMvc mvc;
+
+    @Autowired
+    private MessageRestController msgController;
+
+    @Test
+    public void getMsg() throws Exception {
+        String token = "215e5908cc77a8ee";
+
+    }
 }
