88880dfb2d27eb1caf8331c1955ba98cf211cdc5
Piotr <porleanski@outlook.com>
2019-01-07 20:48:33 +0100

    Remove unused field from map

--- Arabella/src/main/java/arabella/backend/model/Map.java
+++ Arabella/src/main/java/arabella/backend/model/Map.java
@@ -24,10 +24,6 @@ public class Map {
     @NotNull
     private Long lessonId;
 
-    public Long getId() {
-        return id;
-    }
-
     public void setId(Long id) {
         this.id = id;
     }
