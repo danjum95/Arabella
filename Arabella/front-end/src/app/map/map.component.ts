3b378b783d9cd1410adfc235dd4a0f6433bec8c6
Dawid Majsnerowski <dawid.majsnerowski@wp.pl>
2019-01-13 19:36:38 +0100

    Map

--- Arabella/front-end/src/app/map/map.component.ts
+++ Arabella/front-end/src/app/map/map.component.ts
@@ -147,8 +147,8 @@ export class MapComponent implements OnInit {
               })
           });
         this.view = new OlView({
-          center: fromLonLat([16.9335199, 52.4082663]),
-          zoom: 15
+          center: fromLonLat([places[0][0],  places[0][1]]),
+          zoom: 12
         });
 
 
