908fbdfe6deb41c05f4e6445a07e52c8b9f7be23
Dawid Majsnerowski <dawid.majsnerowski@wp.pl>
2019-01-15 09:01:11 +0100

    Fix

--- Arabella/front-end/src/app/map/map.component.ts
+++ Arabella/front-end/src/app/map/map.component.ts
@@ -55,7 +55,7 @@ export class MapComponent implements OnInit {
         for (var i = 0; i < da.length; i++)
         {
           this.lessonsId.push(da[i].id);
-          this.getMap(this.lessonsId[i]);
+          this.getMap(this.lessonsId[this.lessonsId.length - 1]);
           //console.log(this.lessonsId[i]);
         }
       })
